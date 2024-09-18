63929dd998abe9fe1ff867dabe4ef4ea9b30499b
Ice Bear <admin@geode.fi>
2024-09-18 08:34:14 +0300

    feat: move fetch_proposed_pubkeys to portal helpers, renamed to get_validators_batch

--- src/helpers/portal.py
+++ src/helpers/portal.py
@@ -6,6 +6,7 @@ from typing import Iterable
 from web3.types import EventData
 from web3.contract.contract import ContractEvent
 
+from src.utils.thread import multithread
 from src.globals import get_logger, get_sdk
 from src.helpers.events import get_all_events
 
@@ -20,8 +21,8 @@ def get_StakeParams() -> list:
     return get_sdk().portal.functions.StakeParams().call()
 
 
-def fetch_proposed_pubkeys(first_block, last_block) -> list[str]:
-    """Fetches the list of proposed pubkeys by checking the event
+def get_proposed_pubkeys(first_block, last_block) -> list[str]:
+    """Get the list of proposed pubkeys by checking the event
         named StakeProposal from Portal, between given block range.
 
     Args:
@@ -43,3 +44,56 @@ def fetch_proposed_pubkeys(first_block, last_block) -> list[str]:
     flattened_pks = [pubkey for event in detected_events for pubkey in event.args.pubkeys]
 
     return flattened_pks
+
+
+def get_validator(pubkey: str) -> dict:
+    """Returns the data for a validator with the given pubkey.
+    Only processes the Portal information, leaves the Beacon chain related ones for later.
+    Since the deposits might not be processed at the moment.
+    Note that the Portal data gathered here, never changes,
+    so it is safe to assume the latest is up to date.
+
+    Args:
+        pubkey (str): public key of the validator
+
+    Returns:
+        dict: dictionary containing the gathered validator info
+    """
+    # TODO: delete this.
+    # Although all of the required data for the validators should be available,
+    # the following ones might not yet since the deposit can be still not yet processed.
+    # So, instead of not processing them, we will create the indexes
+    # but fill them later when deposits are being processed.
+
+    val = get_sdk().portal.validator(pubkey)
+    return {
+        "pubkey": pubkey,
+        "portal_index": val.portal_index,
+        "portal_state": val.portal_state,
+        "pool_id": val.poolId,
+        "operator_id": val.operatorId,
+        "pool_fee": val.poolFee,
+        "operator_fee": val.operatorFee,
+        "infrastructure_fee": val.infrastructureFee,
+        "signature31": val.signature31,
+        "beacon_index": None,
+        "beacon_status": None,
+        "withdrawal_credentials": None,
+        "exit_epoch": None,
+        "beacon_balance": 0,
+        "withdrawn_balance": 0,
+        "fee_recipient_balance": 0,
+    }
+
+
+def get_validators_batch(pks: list[str]) -> list[dict]:
+    """Fetches the data for validators within the given pks list. Returns the gathered data.
+
+    Args:
+        pks (list[str]): pubkeys that will be fetched
+
+    Returns:
+        list[dict]: list of dictionaries containing the validator info
+    """
+
+    return multithread(get_validator, pks)
